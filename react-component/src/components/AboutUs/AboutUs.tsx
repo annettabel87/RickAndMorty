39e4412864ff21d79775b46a7fc8982e91e4d5d9
annettabel87 <annettabel@rambler.ru>
2022-03-27 01:14:08 +0500

    feat: add style to aboutUs page

--- react-component/src/components/AboutUs/AboutUs.tsx
+++ react-component/src/components/AboutUs/AboutUs.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './AboutUs.css';
 
 export class AboutUs extends React.Component {
   render() {
-    return <h1>AboutUs</h1>;
+    return (
+      <div className="aboutUs-wrapper">
+        <h2 className="aboutUs-title">About Us</h2>
+      </div>
+    );
   }
 }
