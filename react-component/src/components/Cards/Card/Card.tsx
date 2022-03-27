c20b32bd68fa62c2fcf7280d3d8b52854a3db927
annettabel87 <annettabel@rambler.ru>
2022-03-28 01:12:20 +0500

    feat: add cards

---
+++ react-component/src/components/Cards/Card/Card.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Card.css';
+import { ICardData } from '../Cards';
+
+export class Card extends React.Component<ICardData> {
+  constructor(props: ICardData) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="card">
+        <div className="card--img-wrapper">
+          <img
+            className="card-img"
+            src={require(`../../../assets/cardsImg/${this.props.id}.jpg`)}
+            alt="image"
+          />
+        </div>
+        <div className="card--description-wrapper">
+          <div className="card--description-item">#{this.props.id}</div>
+          <div className="card--description-item">Details: {this.props.details}</div>
+          <div className="card--description-item">Age: {this.props.age}</div>
+          <div className="card--description-item price">Price: {this.props.price}</div>
+        </div>
+      </div>
+    );
+  }
+}
--- react-component/src/components/Cards/Cards.tsx
+++ react-component/src/components/Cards/Cards.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { ReactComponentElement, ReactElement, ReactText } from 'react';
+import cardsData from '../../cardsData.json';
+import { Card } from './Card/Card';
 import './Cards.css';
 
+export interface ICardData {
+  id: number;
+  name: string;
+  price: number;
+  age: string;
+  details: number;
+}
 export class Cards extends React.Component {
+  cardsElement = cardsData.map((data: ICardData) => <Card key={data.id.toString()} {...data} />);
+
   render() {
-    return <h1>Cards</h1>;
+    return <div className="cardsPage">{this.cardsElement}</div>;
   }
 }
