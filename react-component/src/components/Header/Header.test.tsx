f40c0dc6e2986ddd5fc3118b62a377ac65867330
annettabel87 <annettabel@rambler.ru>
2022-04-06 00:39:06 +0500

    fix: fix header test

--- react-component/src/components/Header/Header.test.tsx
+++ react-component/src/components/Header/Header.test.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { Header } from './Header';
+import { MemoryRouter } from 'react-router-dom';
 
 test('render header', () => {
-  render(<Header />);
-  // const linkElement = screen.getByRole('');
-  // expect(linkElement).toBeInTheDocument();
+  render(
+    <MemoryRouter>
+      <Header />
+    </MemoryRouter>
+  );
+  const mainLinkElement = screen.getByText(/main/i);
+  expect(mainLinkElement).toBeInTheDocument();
+  const aboutLinkElement = screen.getByText(/about/i);
+  expect(aboutLinkElement).toBeInTheDocument();
+  const cardsLinkElement = screen.getByText(/cards/i);
+  expect(cardsLinkElement).toBeInTheDocument();
+  const formLinkElement = screen.getByText(/form/i);
+  expect(formLinkElement).toBeInTheDocument();
 });
