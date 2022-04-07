4cd55ff60ce3ecf17169df9c1256ff1cdace9ae2
annettabel87 <annettabel@rambler.ru>
2022-04-07 22:45:24 +0500

    fix: add path from header to constants

--- react-component/src/components/Header/Header.tsx
+++ react-component/src/components/Header/Header.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Header.css';
+import { routers } from '../../constants';
 
 export class Header extends React.Component {
   render() {
     return (
       <header className="header">
-        <NavLink className="header-item" to="/">
+        <NavLink className="header-item" to={routers.ROUTE_MAIN}>
           Main
         </NavLink>
-        <NavLink className="header-item" to="/about">
+        <NavLink className="header-item" to={routers.ROUTE_ABOUT}>
           About Us
         </NavLink>
-        <NavLink className="header-item" to="/cards">
+        <NavLink className="header-item" to={routers.ROUTE_CARDS}>
           Cards
         </NavLink>
-        <NavLink className="header-item" to="/form">
+        <NavLink className="header-item" to={routers.ROUTE_FORM}>
           Form
         </NavLink>
       </header>
