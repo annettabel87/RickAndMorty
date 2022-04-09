371db9acdbf6f617ac1d9b9b1cfd7b84030bf6e4
annettabel87 <annettabel@rambler.ru>
2022-04-10 00:58:41 +0500

    feat: add loader

--- react-component/src/components/Main/Main.tsx
+++ react-component/src/components/Main/Main.tsx
@@ -4,6 +4,7 @@ import { EmptyProps } from '../FormPage/FormPage';
 import { apiConstants } from '../../constants';
 import { CardsField } from './Cards/CardsField';
 import './Main.css';
+import { Loader } from './Loader/Loader';
 
 export interface IRickAndMortyData {
   id: number;
@@ -29,6 +30,7 @@ type SearchState = {
   data: IRickAndMortyData[];
   searchValue: string;
   isLoaded: boolean;
+  error: boolean;
 };
 export interface ISearchProps {
   handleChange: (event: ChangeEvent<HTMLInputElement>) => void;
@@ -40,7 +42,7 @@ export class Main extends React.Component<EmptyProps, SearchState> {
   state: SearchState;
   constructor(props = {}) {
     super(props);
-    this.state = { data: [], searchValue: '', isLoaded: false };
+    this.state = { data: [], searchValue: '', isLoaded: false, error: false };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.reset = this.reset.bind(this);
@@ -68,14 +70,18 @@ export class Main extends React.Component<EmptyProps, SearchState> {
     this.setState({ searchValue: event.target.value.toLowerCase() });
   }
   async handleSubmit(e: React.ChangeEvent<HTMLFormElement>) {
+    this.setState({ error: false });
     e.preventDefault();
     this.setState({ isLoaded: true });
     try {
       const response = await fetch(`${apiConstants.characterUrl}/?name=${this.state.searchValue}`);
       const data = await response.json();
+      if (!response.ok) {
+        throw Error('not found');
+      }
       this.setState({ data: data.results });
     } catch (err) {
-      console.log(err);
+      this.setState({ error: true });
     } finally {
       this.setState({ isLoaded: false });
       this.setState({ searchValue: '' });
@@ -96,7 +102,14 @@ export class Main extends React.Component<EmptyProps, SearchState> {
           reset={this.reset}
           searchValue={this.state.searchValue}
         />
-        <CardsField {...this.state.data} />
+        {this.state.error && <span className="mainPage-error">not found</span>}
+        {this.state.isLoaded ? (
+          <div className="loader">
+            <Loader />
+          </div>
+        ) : (
+          <CardsField {...this.state.data} />
+        )}
       </div>
     );
   }
