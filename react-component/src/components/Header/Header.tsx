65a5678ce76e84b0a2d9d97b9c13a2026e3fcfe4
annettabel87 <annettabel@rambler.ru>
2022-04-15 23:08:51 +0500

    refactor: rewrite to hooks Header

--- react-component/src/components/Header/Header.tsx
+++ react-component/src/components/Header/Header.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { NavLink } from 'react-router-dom';
 import './Header.css';
 import { routers } from '../../constants';
 
-export class Header extends React.Component {
-  render() {
-    return (
-      <header className="header">
-        <NavLink className="header-item" to={routers.ROUTE_MAIN}>
-          Main
-        </NavLink>
-        <NavLink className="header-item" to={routers.ROUTE_ABOUT}>
-          About Us
-        </NavLink>
-        <NavLink className="header-item" to={routers.ROUTE_CARDS}>
-          Cards
-        </NavLink>
-        <NavLink className="header-item" to={routers.ROUTE_FORM}>
-          Form
-        </NavLink>
-      </header>
-    );
-  }
-}
+export const Header: FC = () => {
+  return (
+    <header className="header">
+      <NavLink className="header-item" to={routers.ROUTE_MAIN}>
+        Main
+      </NavLink>
+      <NavLink className="header-item" to={routers.ROUTE_ABOUT}>
+        About Us
+      </NavLink>
+      <NavLink className="header-item" to={routers.ROUTE_CARDS}>
+        Cards
+      </NavLink>
+      <NavLink className="header-item" to={routers.ROUTE_FORM}>
+        Form
+      </NavLink>
+    </header>
+  );
+};
